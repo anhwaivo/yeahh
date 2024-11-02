8c7265f70b5e265681302db103a966b897fd17cf
anhwaivo <97143764+anhwaivo@users.noreply.github.com>
2024-11-02 12:25:50 +0700

    Update updateconf.sh

--- updateconf.sh
+++ updateconf.sh
@@ -1,1 +1,1 @@
-cd /data/data/com.termux/files/usr/bin/ && wget -O config.json https://raw.githubusercontent.com/anhwaivo/yeahh/refs/heads/main/config.json
+cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && wget -O config.json https://raw.githubusercontent.com/anhwaivo/yeahh/refs/heads/main/config.json
